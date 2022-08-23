d29fbbc42d043b91c259367c6f591fcaf2272f05
gabriel cahu-penhirin <gabriel.cahu-penhirin@atos.net>
2022-08-23 15:03:54 +0200

    ui

--- src/leaflet.trackplayback/trackplayback.js
+++ src/leaflet.trackplayback/trackplayback.js
@@ -104,11 +104,6 @@ export const TrackPlayBack = L.Class.extend({
   },
   removeDraw: function () {
     this.draw.remove()
-    this.tracks = null
-    this.draw = null
-    this.trackController = null
-    this.clock.off('tick', this._tick)
-    this.clock = null
   },
   dispose: function () {
     this.clock.off('tick', this._tick)
